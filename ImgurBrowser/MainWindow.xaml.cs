ed28673c4af2ff9d662570cab74e9bf40407f252
Miguel <luis.miguel.ibarra@gmail.com>
2014-07-05 18:16:39 -0500

    listbox showing without margins

--- ImgurBrowser/MainWindow.xaml.cs
+++ ImgurBrowser/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Miq.ImgurBrowser.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +17,52 @@ using System.Windows.Shapes;
 
 namespace Miq.ImgurBrowser
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupImageLoader();
+        }
+        
+        void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Images = ImageList.DataContext as ImageCollection;
+            LoadImages();
+        }
+
+        #region Loading Images
+        void SetupImageLoader()
+        {
+            ImageLoader = new BackgroundWorker();
+            ImageLoader.DoWork += ImageLoader_DoWork;
+            ImageLoader.ProgressChanged += ImageLoader_ProgressChanged;
+            ImageLoader.WorkerReportsProgress = true;
+        }
+
+        private void LoadImages()
+        {
+            ImageLoader.RunWorkerAsync(Images);
+        }
+        
+        void ImageLoader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ImageCollection images = e.Argument as ImageCollection;
+            Data.Image nextImage;
+            while ((nextImage = images.GetNextImage()) != null)
+            {
+                ImageLoader.ReportProgress(0, nextImage);
+            }
+        }
+        
+        private void ImageLoader_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Data.Image image = e.UserState as Data.Image;
+            Images.Insert(0, image);
+        }
+
+        BackgroundWorker ImageLoader;
+        ImageCollection Images;
+        #endregion
+    }
 }
