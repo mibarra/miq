4af00d3d36a045127b7b689f5488c514de5a2cfe
Miguel Ibarra <luis.miguel.ibarra@gmail.com>
2014-07-25 18:47:11 -0500

    cleaning m2k

---
+++ Miq.Tests/Nursery/WmiUsbTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Management;
+
+namespace Miq.Tests.Nursery
+{
+	[TestClass]
+	public class WmiUsbTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			System.Management.ManagementClass USBClass = new ManagementClass("Win32_USBDevice");
+			System.Management.ManagementObjectCollection USBCollection = USBClass.GetInstances();
+
+			foreach (System.Management.ManagementObject usb in USBCollection)
+			{
+				try
+				{
+					string deviceId = usb["deviceid"].ToString();
+					string a0 = usb["Caption"].ToString();
+					string a1 = usb["Description"].ToString();
+					string a2 = usb["Manufacturer"].ToString();
+					string a3 = usb["Name"].ToString();
+					string a4 = usb["PNPDeviceID"].ToString();
+				}
+				catch (Exception)
+				{
+				}
+			}
+		}
+	}
+}
