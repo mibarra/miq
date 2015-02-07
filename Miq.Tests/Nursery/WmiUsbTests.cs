483d1dc8ae2d876c7df7864e1958a1590263aede
Miguel Ibarra <luis.miguel.ibarra@gmail.com>
2015-02-07 17:24:36 -0600

    Fixed two red tests

--- Miq.Tests/Nursery/WmiUsbTests.cs
+++ Miq.Tests/Nursery/WmiUsbTests.cs
@@ -4,30 +4,31 @@ using System.Management;
 
 namespace Miq.Tests.Nursery
 {
-	[TestClass]
-	public class WmiUsbTests
-	{
-		[TestMethod]
-		public void TestMethod1()
-		{
-			System.Management.ManagementClass USBClass = new ManagementClass("Win32_USBDevice");
-			System.Management.ManagementObjectCollection USBCollection = USBClass.GetInstances();
+    [TestClass]
+    public class WmiUsbTests
+    {
+        [TestMethod]
+        [Ignore] // ZZZ test is no testing anything
+        public void TryingToGetListOfUSBDevices()
+        {
+            System.Management.ManagementClass USBClass = new ManagementClass("Win32_USBDevice");
+            System.Management.ManagementObjectCollection USBCollection = USBClass.GetInstances();
 
-			foreach (System.Management.ManagementObject usb in USBCollection)
-			{
-				try
-				{
-					string deviceId = usb["deviceid"].ToString();
-					string a0 = usb["Caption"].ToString();
-					string a1 = usb["Description"].ToString();
-					string a2 = usb["Manufacturer"].ToString();
-					string a3 = usb["Name"].ToString();
-					string a4 = usb["PNPDeviceID"].ToString();
-				}
-				catch (Exception)
-				{
-				}
-			}
-		}
-	}
+            foreach (System.Management.ManagementObject usb in USBCollection)
+            {
+                try
+                {
+                    string deviceId = usb["deviceid"].ToString();
+                    string a0 = usb["Caption"].ToString();
+                    string a1 = usb["Description"].ToString();
+                    string a2 = usb["Manufacturer"].ToString();
+                    string a3 = usb["Name"].ToString();
+                    string a4 = usb["PNPDeviceID"].ToString();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
 }
